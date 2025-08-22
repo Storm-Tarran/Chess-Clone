1a9d060fceb3eac82563d4f35d86392958a0ccfb
Storm Tarran <stormtarran03@gmail.com>
2025-08-22 11:46:06 +0200

    Added in the images of the chess pieces, fixed quality of pieces and board.

---
+++ ChessUI/Images.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ChessLogic;
+
+namespace ChessUI
+{
+    static class Images
+    {
+        //Load all images
+        private static readonly Dictionary<PieceType, ImageSource> whitePieces = new()
+        {
+            { PieceType.Pawn, ImageSource("Assest/PawnW.png") },
+            { PieceType.Rook, ImageSource("Assest/RookW.png") },
+            { PieceType.Knight, ImageSource("Assest/KnightW.png") },
+            { PieceType.Bishop, ImageSource("Assest/BishopW.png") },
+            { PieceType.Queen, ImageSource("Assest/QueenW.png") },
+            { PieceType.King, ImageSource("Assest/KingW.png") }
+        };
+
+        private static readonly Dictionary<PieceType, ImageSource> blackPieces = new()
+        {
+            { PieceType.Pawn, ImageSource("Assest/PawnB.png") },
+            { PieceType.Rook, ImageSource("Assest/RookB.png") },
+            { PieceType.Knight, ImageSource("Assest/KnightB.png") },
+            { PieceType.Bishop, ImageSource("Assest/BishopB.png") },
+            { PieceType.Queen, ImageSource("Assest/QueenB.png") },
+            { PieceType.King, ImageSource("Assest/KingB.png") }
+        };
+
+        //Loain
+        private static ImageSource ImageSource(string imageFileName)
+        {
+            return new BitmapImage(new Uri(imageFileName, UriKind.Relative));
+        }
+
+        //Get the image based on color and type
+        public static ImageSource GetImageSource(Player color, PieceType type)
+        {
+            return color switch
+            {
+                Player.White => whitePieces[type],
+                Player.Black => blackPieces[type],
+                _ => null
+            };
+        }
+
+        public static ImageSource GetImageSource(Piece piece)
+        {
+            if (piece == null)
+            {
+                return null;
+            }
+            return GetImageSource(piece.Color, piece.Type);
+        }
+    }
+}
--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLogic;
 
 namespace ChessUI
 {
@@ -16,9 +17,45 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        //2D Array for the chessboard, 8x8
+        private readonly Image[,] chessboardImages = new Image[8, 8];
+
+        private GameState gameState;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            //loop over all positions on the chessboard
+            for(int row = 0; row < 8; row++ )
+            {
+                for(int col = 0; col < 8; col++)
+                {
+                    Image chessImage = new Image();
+                    chessboardImages[row, col] = chessImage;
+                    PieceGrid.Children.Add(chessImage);
+                }
+            }
+        }
+
+        //Method to draw board
+        public void DrawBoard(Board board)
+        {
+            for(int row = 0; row < 8; row++)
+            {
+                for(int col = 0; col < 8; col++)
+                {
+                    //Grab the piece aat that position
+                    Piece piece = board[row, col];
+                    chessboardImages[row, col].Source = Images.GetImageSource(piece);
+                }
+            }
         }
     }
 }
